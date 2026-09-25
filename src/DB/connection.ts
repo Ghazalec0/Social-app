9f661da14486391b244ac836eb6a827bc901c9ab
Abdulrahman Taha <ghazalec027@gmail.com>
2026-09-25 20:17:20 +0300

    fix ts global type

--- src/DB/connection.ts
+++ src/DB/connection.ts
@@ -1,9 +1,35 @@
 import mongoose from "mongoose";
 import { DB_URI } from "../config";
 
-export function connectDB() {
-    mongoose
-    .connect(DB_URI)
-    .then(() => console.log("DB Connected successfully"))
-    .catch((err) => console.log("fail to connect to DB", err.message));
+let cached = (global as any).mongoose;
+
+if (!cached) {
+  cached = (global as any).mongoose = { conn: null, promise: null };
+}
+
+export async function connectDB() {
+  if (cached.conn) {
+    return cached.conn;
+  }
+
+  if (!cached.promise) {
+    const opts = {
+      bufferCommands: false,
+    };
+
+    cached.promise = mongoose.connect(DB_URI, opts).then((mongoose) => {
+      console.log("DB Connected successfully");
+      return mongoose;
+    });
+  }
+  
+  try {
+    cached.conn = await cached.promise;
+  } catch (e: any) {
+    cached.promise = null;
+    console.log("fail to connect to DB", e.message);
+    throw e;
+  }
+
+  return cached.conn;
 }
