b8fcf654afa477a240b3edab79571adf935861c9
Abdulrahman Taha <ghazalec027@gmail.com>
2026-09-25 17:38:36 +0300

    fix firebase for vercel

--- src/common/notification/firebase/init.ts
+++ src/common/notification/firebase/init.ts
@@ -2,17 +2,17 @@ import { FirebasePushNotificationProvider } from "./firebase.service";
 import * as fs from "node:fs";
 import path from "node:path";
 
-const config: any = JSON.parse(
-    fs.readFileSync(
-        path.join(
-            process.cwd(),
-            "src",
-            "config",
-            "social-app-c45-g3-2f8e5-firebase-adminsdk-fbsvc-83073dd2ff.json"
-        ),
-        "utf-8"
-    )
-);
+let config: any;
 
-export const firebasePushNotificationProvider =
-    new FirebasePushNotificationProvider(config);
+if (process.env.FIREBASE_SERVICE_ACCOUNT) {
+  config = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+} else {
+  config = JSON.parse(
+    fs.readFileSync(
+      path.join(process.cwd(), "src", "config", "social-app-c45-g3-2f8e5-firebase-adminsdk-fbsvc-83073dd2ff.json"),
+      "utf-8"
+    )
+  );
+}
+
+export const firebasePushNotificationProvider = new FirebasePushNotificationProvider(config);
